9bd930bb47cf3b0d422874237fda01e4fbf90923
Sara Ryfczyńska <s.ryfczynska@gmail.com>
2020-02-18 15:24:15 +0100

    Update src/data/file_manager/file_creator.py
    Co-Authored-By: Marcin Bodych <bodychmarcin@gmail.com>

--- src/data/file_manager/file_creator.py
+++ src/data/file_manager/file_creator.py
@@ -8,6 +8,6 @@ class FileCreator:
     def create_json_file(self, data, file_name):
         json_data = json.dumps(data)
 
-        full_path = "{}/{}.json".format(self._output_path, file_name)
+        full_path = os.path.join(self._output_path, '{}.json'.format(file_name))
         with open(full_path, 'w') as output_file:
             output_file.write(json_data)
