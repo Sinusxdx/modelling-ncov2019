30900b958f67ecaee189345125c773b64323f15b
onomatopeia <agata.migalska@gmail.com>
2020-02-24 05:05:31 +0100

    Updated households with representants info

--- src/data/poland.py
+++ src/data/poland.py
@@ -4,15 +4,18 @@ from pathlib import Path
 
 families_per_household_xlsx = 'households_with_families.xlsx'
 household_family_structure_xlsx = 'household_family_structure.xlsx'
+household_family_structure_old_xlsx = 'household_family_structure_old.xlsx'
+households_count_xlsx = 'households_count.xlsx'
 
 
-def prepare_family_structure_from_voivodship(data_folder):
+def prepare_family_structure_from_voivodship_old(data_folder):
     family_structure_input_sheet = 'processed'
     voivodship = os.path.basename(data_folder)[0]
     families_per_household_df = pd.read_excel(os.path.join(data_folder, families_per_household_xlsx))
 
-    family_structure_df = pd.read_excel(os.path.join(data_folder, os.pardir, voivodship, household_family_structure_xlsx),
-                                        sheet_name=family_structure_input_sheet)
+    family_structure_df = pd.read_excel(
+        os.path.join(data_folder, os.pardir, voivodship, household_family_structure_old_xlsx),
+        sheet_name=family_structure_input_sheet)
 
     columns = family_structure_df.columns[1:]
     for col in columns:
@@ -20,14 +23,26 @@ def prepare_family_structure_from_voivodship(data_folder):
             family_structure_df[col] * families_per_household_df.loc[0, col] / family_structure_df[col].sum())\
             .astype(int)
 
-    family_structure_df.to_excel(os.path.join(data_folder, household_family_structure_xlsx), index=False)
+    family_structure_df.to_excel(os.path.join(data_folder, household_family_structure_old_xlsx), index=False)
 
 
-def generate_household_indices(data_folder):
+def prepare_family_structure_from_voivodship(data_folder):
+    voivodship = os.path.basename(data_folder)[0]
+    voivodship_folder = os.path.join(data_folder, os.pardir, voivodship)
+    df = pd.read_excel(os.path.join(voivodship_folder, household_family_structure_xlsx), sheet_name='processed')
+    df2 = pd.melt(df,
+                  id_vars=['family_type', 'relationship', 'house master', 'family_structure_regex'],
+                  value_vars=[1, 2, 3, 4, 5, 6, 7], var_name='household_headcount',
+                  value_name='probability_within_headcount')
+
+    df2.to_excel(os.path.join(data_folder, household_family_structure_xlsx), index=False)
+
+
+def generate_household_indices_old(data_folder):
     headcount2 = []
     family_type = []
 
-    family_structure_df = pd.read_excel(os.path.join(data_folder, household_family_structure_xlsx))
+    family_structure_df = pd.read_excel(os.path.join(data_folder, household_family_structure_old_xlsx))
 
     for i, row in family_structure_df.iterrows():
         headcount2.extend([row['Household size']] * int(row['One family']))
@@ -43,11 +58,43 @@ def generate_household_indices(data_folder):
 
     household_df = pd.DataFrame(
         data={'headcount': headcount2, 'family_type': family_type, 'household_index': list(range(len(headcount2)))})
+    household_df.set_index('household_index').to_excel(os.path.join(data_folder, 'households_old.xlsx'))
+
+
+def generate_household_indices(data_folder):
+    household_headcount = []
+    family_type = []
+    relationship = []
+    house_master = []
+    family_structure_regex = []
+
+    family_structure_df = pd.read_excel(os.path.join(data_folder, household_family_structure_xlsx))
+    households_count_df = pd.read_excel(os.path.join(data_folder, households_count_xlsx))
+
+    for i, hc_row in households_count_df.iterrows():
+
+        df = family_structure_df[family_structure_df.household_headcount == hc_row.nb_of_people_in_household].copy()
+        df['total'] = (df.probability_within_headcount * hc_row.nb_of_households).astype(int)
+        for j, row in df.iterrows():
+            if row.total > 0:
+                household_headcount.extend([row.household_headcount] * row.total)
+                family_type.extend([row.family_type] * row.total)
+                relationship.extend([row.relationship] * row.total)
+                house_master.extend([row.house_master] * row.total)
+                family_structure_regex.extend([row.family_structure_regex] * row.total)
+
+    household_df = pd.DataFrame(data=dict(household_index=list(range(len(household_headcount))),
+                                          household_headcount=household_headcount,
+                                          family_type=family_type,
+                                          relationship=relationship,
+                                          house_master=house_master,
+                                          family_structure_regex=family_structure_regex))
+
     household_df.set_index('household_index').to_excel(os.path.join(data_folder, 'households.xlsx'))
 
 
 if __name__ == '__main__':
     project_dir = Path(__file__).resolve().parents[2]
     data_folder = os.path.join(project_dir, 'data', 'processed', 'poland', 'DW')
-    prepare_family_structure_from_voivodship(data_folder)
-    generate_household_indices(data_folder)
+    # prepare_family_structure_from_voivodship(data_folder)
+    # generate_household_indices(data_folder)
--- src/features/population.py
+++ src/features/population.py
@@ -1,24 +1,16 @@
 from typing import List, Callable, Dict, Tuple, Any
-import networkx as nx
+import pandas as pd
+import os
 
-Node_Type = Tuple[Any, Dict[str, Any]]
 
-
-def generate(population_size: int, decorators: List[Tuple[Callable[..., Node_Type], Tuple[Any]]]) -> nx.MultiGraph:
+def generate(data_folder) -> pd.DataFrame:
     """
-    :param population_size: size of a population to generate
-    :param decorators: list of tuples, where each tuple consists of a callable (a function) and a tuple of
-    additional arguments that should be passed to this function. The signature of the function should be similar to
-    this one:
-    ``` def function_name(node: Node_Type, *args: Any) -> Node_Type ```
-    :return: an undirected graph of type nx.MultiGraph
+    :return: an dataframe with population generated from data in data_folder
     """
+    voivodship = os.path.basename(data_folder)[0]
+    voivodship_folder = os.path.join(data_folder, os.pardir, voivodship)
+    pass
 
-    # fake implementation
-    p = 1/6
-    graph = nx.fast_gnp_random_graph(population_size, p, directed=False)
-    for node in graph.nodes(data=True):
-        for decorator, args in decorators:
-            node = decorator(node, *args)
-    return graph
+
+
 
